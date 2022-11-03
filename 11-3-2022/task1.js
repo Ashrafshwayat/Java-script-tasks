7c4b479ba75b4d2128bc16602818cba7ab696089
Your Name <you@example.com>
2022-11-03 15:58:48 +0300

    assss

---
+++ 11-3-2022/task1.js
@@ -0,0 +1,23 @@
+var x=40;
+if (x>100){
+    document.write("invalid data"+"<br>")
+}
+else if (x<50){
+    document.write("fail"+"<br>")
+}
+else if (x>=50&&x<=59){
+    document.write("D"+"<br>")
+}
+else if (x>59&&x<=69){
+    document.write("C"+"<br>")
+}
+else if (x>69&&x<=79){
+    document.write("B"+"<br>")
+}
+else if (x>79&&x<=89){
+    document.write("A"+"<br>")
+}
+else{
+    document.write("A+"+"<br>")
+}
+
---
+++ 11-3-2022/task2.js
@@ -0,0 +1,3 @@
+const x=3.14
+z=x*15
+document.write (z);
