77803ceefeff9c9cf7a1e462e3d8b80f2498ba99
Your Name <you@example.com>
2022-11-06 20:19:03 +0300

    loop

---
+++ 6-11-2022/task1.js
@@ -0,0 +1,46 @@
+let month= prompt("please enter the month");
+month=Number(month);
+switch(month)
+{
+    case 1:
+      month= "jan";
+       break;
+
+    case 2:
+        month= "fab";
+       break;
+      case 3:
+        month = "March";
+        break;
+      case 4:
+        month = "April";
+        break;
+      case 5:
+        month = "May";
+        break;
+        case 6:
+        month = "Jun";
+        break;
+      case 7:
+        month = "July";
+        break;
+      case  8:
+        month = "Aug";
+        break;
+        case  9:
+        month = "Sep";
+        break;
+        case  10:
+        month = "Oct";
+        break;
+        case  11:
+        month = "Nov";
+        break;
+        case  12:
+        month = "Dec";
+        break;
+        default: month = "Invalid month";
+
+
+}
+ document.write(month)
---
+++ 6-11-2022/task2.js
@@ -0,0 +1,11 @@
+let counter=1;
+for(let row=1;row<=4;row++){
+
+    for(let column=1;column<=row;column++)
+    { document.writeln(counter);
+        counter+=1;
+
+    }
+    document.write("<br>");
+
+}
